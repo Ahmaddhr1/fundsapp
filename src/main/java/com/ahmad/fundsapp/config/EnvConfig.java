54f988c677ef68031875fbf78dfc9bf5abfaf164
ahmaddhr <ahmaddaherr004@gmail.com>
2025-10-17 00:26:48 +0300

    changes to EnvConfig

--- src/main/java/com/ahmad/fundsapp/config/EnvConfig.java
+++ src/main/java/com/ahmad/fundsapp/config/EnvConfig.java
@@ -3,11 +3,28 @@ package com.ahmad.fundsapp.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class EnvConfig {
-    private static final Dotenv dotenv = Dotenv.load();
 
-    public static String get(String key) {
-        return dotenv.get(key);
+    private final Dotenv dotenv;
+
+    public EnvConfig() {
+        Dotenv temp = null;
+        try {
+            temp = Dotenv.load();
+        } catch (Exception e) {
+        }
+        this.dotenv = temp;
+    }
+
+    public String get(String key) {
+        if (dotenv != null && dotenv.get(key) != null) {
+            return dotenv.get(key);
+        }
+        String envValue = System.getenv(key);
+        if (envValue == null) {
+            throw new RuntimeException("Environment variable " + key + " not set!");
+        }
+        return envValue;
     }
 }
