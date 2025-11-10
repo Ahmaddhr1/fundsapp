5a7098f046e4138f95dfdb829055e653c726efc9
ahmaddhr <ahmaddaherr004@gmail.com>
2025-11-10 23:53:11 +0200

    back to pooler  connection "supabase refusing the url provided from render but working perfectly on my machine!"

--- src/main/java/com/ahmad/fundsapp/FundsappApplication.java
+++ src/main/java/com/ahmad/fundsapp/FundsappApplication.java
@@ -8,11 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundsappApplication {
 
     public static void main(String[] args) {
-//        Dotenv dotenv = Dotenv.load();
-//
-//        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-//        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-//        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(FundsappApplication.class, args);
     }
 
